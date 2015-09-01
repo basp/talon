713d772fe740b26eb16aa461c127eebeb7f1c5b2
Bas Pennings <baspster@gmail.com>
2015-09-01 17:01:46 +0200

    Just some toying around

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world!');
+module Talon {
+	export interface Object {
+		id(): number;
+		location(): Object;
+		contents(): Object[];		
+	}	
+}
+
+var obj = <Talon.Object>{
+	id: () => 123,
+	location: () => undefined,
+	contents: () => []
+};
+
+console.log(obj.contents());
